bd1c7e9b1e6750ed7c08f692e39ff69ccdcf854c
Iamozgurenes <ensosnz@gmail.com>
2024-10-25 13:11:57 +0300

    updatre

---
+++ my-next-app/src/components/SearchBar.tsx
@@ -0,0 +1,27 @@
+// src/components/SearchBar.tsx
+import { useState } from 'react';
+
+export default function SearchBar({ onSearch }:any) {
+    const [query, setQuery] = useState('');
+
+    const handleSearch = (e:any) => {
+        e.preventDefault();
+        onSearch(query);
+    };
+
+    return (
+        <form onSubmit={handleSearch} className="mb-4">
+            <input
+                type="text"
+                placeholder="Ülke ara..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className="border border-gray-300 rounded p-2 w-full"
+            />
+            <button type="submit" className="mt-2 bg-blue-500 text-white rounded p-2 w-full">
+                Ara
+            </button>
+        </form>
+    );
+}
+
